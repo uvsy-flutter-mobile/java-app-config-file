1274035a3f7c7aeab0151e132e70faff0c8d263f
Gonzalo Saad <saad.gonzalo.ale@gmail.com>
2019-06-17 13:17:37 -0500

    Agregando mas output de consola

--- src/main/java/com/universy/Main.java
+++ src/main/java/com/universy/Main.java
@@ -18,25 +18,36 @@ public class Main {
         String stage = args[0];
 
         try {
-            System.out.println("Working on: " + System.getProperty("user.dir"));
 
-            System.out.println("Stage: " + stage);
-            System.out.println("Fetching config from S3 Bucket");
+            String userName = System.getProperty("user.name");
+            String userDir = System.getProperty("user.dir");
+
+            System.out.println(String.format("Hello %s!", userName));
+            System.out.println("We are on: " + userDir);
+
+            System.out.println("Stage you asked: " + stage);
+            System.out.println("Fetching config from S3 Bucket!");
             ConfigFileFetcher fileFetcher = new ConfigFileFetcher(stage);
             String content = fileFetcher.fetch();
 
             if(content.isEmpty()){
-                System.out.println("Config file is empty!");
+                System.out.println("Config file is empty! What? Contact your AWS Admin. :(");
             } else{
                 System.out.println("File retrieved successfully.");
-                System.out.println("Writing file to config folder.");
+                System.out.println("Writing file to config folder. Almost done! :)");
                 ConfigFileWriter writer = new ConfigFileWriter(content);
                 writer.write();
+                System.out.println(String.format("We're done here. Thank you %s!", userName));
+
             }
         } catch (IOException e) {
-            System.out.println("There was an error: " + e.getMessage());
+            System.out.println(String.format("There was an error: %s. Sorry!", e.getMessage()));
         } catch (AmazonS3Exception e){
-            System.out.println("The stage may not be valid. Try another.");
-        }
+            System.out.println(String.format("There was an error with AWS: %s.", e.getMessage()));
+            System.out.println("Possible problems: \n" +
+                    "- Incorrect credentials in .aws/credentials. \n" +
+                    "- Incorrect configuration in .aws/config. \n" +
+                    "- Stage is not correct.");
+         }
     }
 }
